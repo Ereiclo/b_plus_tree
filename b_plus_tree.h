1d38f05b045f8de0398d2f2d6464eb5d7df69bd5
Ereiclo <eric.bracamonte@utec.edu.pe>
2022-03-16 09:59:12 -0500

    feat(b_plus_tree): Declarando los metodos finales para b_plus_tree

--- b_plus_tree.h
+++ b_plus_tree.h
@@ -30,6 +30,41 @@ class b_plus_tree{
   Node* root{nullptr};
 
 
+  void leaf_division_pre(Node*& leaf, T data){}
+  void leaf_division(Node*&leaf){}
+  void node_division(Node*&leaf){}
+  void node_overload(Node*& node, T data, Node* left, Node* right){}
+  void depth_position(Node*& node,T data){}
+  void key_insertion(Node*& node, T data, int pos){}
+  void two_child_insertion(Node*& node, Node* left, Node* right,int pos){}
+
+
+  void insert(T data){}
+
+
+  void remove(T data){}
+
+  void merge(Node*& nodo, Node* left, Node*right,int & depht_pos, T data){}
+  void left_rot(Node*nodo,Node*left,Node* right,int & depht_pos,T data){}
+
+
+  void show_Range(){}
+
+
+  bool find(){ return false;}
+
+
+  void showInOrder(){}
+
+
+  void showPreOrder(){}
+
+
+  void showPostOrder(){}
+
+
+
+
 
 };
 
